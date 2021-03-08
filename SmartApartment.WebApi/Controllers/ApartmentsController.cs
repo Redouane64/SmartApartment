1f9e75c614c04db73a6af87785e7cb37b5d4f275
redouane <redouane.sobaihi@live.com>
2021-03-08 14:15:00 +0700

    Add project files.

---
+++ SmartApartment.WebApi/Controllers/ApartmentsController.cs
@@ -0,0 +1,30 @@
+﻿namespace SmartApartment.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("/")]
+    public class ApartmentsController : ControllerBase
+    {
+
+        private readonly ILogger<ApartmentsController> _logger;
+
+        public ApartmentsController(ILogger<ApartmentsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(nameof(Search), Name = nameof(Search))]
+        public async Task<IActionResult> Search()
+        {
+            return Ok();
+        }
+
+    }
+}
