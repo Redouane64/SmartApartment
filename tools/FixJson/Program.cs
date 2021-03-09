4e53b54b302fd7db9ae285500f5eabf36f9923de
redouane <redouane.sobaihi@live.com>
2021-03-09 23:23:11 +0700

    feat: create uploader tool and use JSON.NET to handle JSON data

--- tools/FixJson/Program.cs
+++ tools/FixJson/Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace FixJson
 {
+
     using Newtonsoft.Json;
+
+    using SampleDataUploader;
 
     using System;
     using System.IO;
@@ -23,6 +26,10 @@
         {
             try
             {
+                WriteLine("This program will your json data.");
+                WriteLine("Press any key to continue...");
+                ReadKey();
+
                 var filename = args[0];
 
                 // replace new line (\r\n) with space and then replace \r with empty character. 
@@ -48,7 +55,7 @@
                 // verify if json is valid and parsable by deserializing the text with JSON.NET
                 if (args[2] == "-m") // for managments data json file
                 {
-                    _ = JsonSerializer.Create().Deserialize<MgmtRoot[]>(new JsonTextReader(new StringReader(json)));
+                    _ = JsonSerializer.Create().Deserialize<ManagementRoot[]>(new JsonTextReader(new StringReader(json)));
                 } else if (args[2] == "-p") // for properties data json file
                 {
                     _ = JsonSerializer.Create().Deserialize<PropertyRoot[]>(new JsonTextReader(new StringReader(json)));
---
+++ tools/SampleDataUploader/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace SampleDataUploader
+{
+    using System;
+    using System.IO;
+
+    using Nest;
+
+    using Newtonsoft.Json;
+
+    using static System.Console;
+
+    class Program
+    {
+        /*
+         * 
+         */
+        static void Main(string[] args)
+        {
+            WriteLine("This program will parse and index your json data to Elasticsearch intance.");
+            WriteLine("Press any key to continue...");
+            ReadKey();
+
+            ConnectionSettings connection = new ConnectionSettings();
+
+            string mgmntJsonFile = args[0];
+            string propertiesJsonFile = args[1];
+
+            if (args.Length > 2)
+            {
+                connection = new ConnectionSettings(new Uri(args[2]));
+            }
+            
+
+            /* push properties and management entities to the same index named `default` */
+            connection.DefaultIndex("default")
+                      .PrettyJson();
+
+            try
+            {
+                var client = new ElasticClient(connection);
+
+                var managements = JsonSerializer.Create().Deserialize<ManagementRoot[]>(
+                    new JsonTextReader(
+                            new StringReader(File.ReadAllText(mgmntJsonFile))
+                        )
+                    );
+
+                var properties = JsonSerializer.Create(new JsonSerializerSettings() { 
+                    FloatParseHandling = FloatParseHandling.Double
+                }).Deserialize<PropertyRoot[]>(
+                    new JsonTextReader(
+                            new StringReader(File.ReadAllText(propertiesJsonFile))
+                        )
+                    );
+
+                ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Error.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
