e5005a1abc3efeb68ccd8174d92e0431a66e9740
redouane <redouane.sobaihi@live.com>
2021-03-12 01:49:26 +0700

    feat: (WIP) implementing search service

---
+++ src/SmartApartment.Common/Abstraction/ISearchService.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartApartment.Common.Abstraction
+{
+    using System.Threading.Tasks;
+    using SmartApartment.Common.Domains;
+
+    public interface ISearchService
+    {
+        Task<Document> Search(string keyword, string market = null, int limit = 25);
+    }
+}
---
+++ src/SmartApartment.Common/Services/SearchService.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartApartment.Common.Services
+{
+    using System;
+    using System.Threading.Tasks;
+    using Nest;
+    using SmartApartment.Common.Abstraction;
+    using SmartApartment.Common.Domains;
+
+    public class SearchService : ISearchService
+    {
+        private readonly IElasticClient client;
+
+        public SearchService(IElasticClient client)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public Task<Document> Search(string keyword, string market = null, int limit = 25)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
