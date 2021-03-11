efe9b07b58f475bde3d655856a5651b8be5e58b5
redouane <redouane.sobaihi@live.com>
2021-03-11 20:14:14 +0700

    fix: fix code and clean up

--- tools/SampleDataUploader/Program.cs
+++ tools/SampleDataUploader/Program.cs
@@ -2,10 +2,15 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     using Nest;
 
     using Newtonsoft.Json;
+
+    using SmartApartment.Common.Domains;
+    using SmartApartment.Common.Services;
 
     using static System.Console;
 
@@ -14,51 +19,91 @@
         /*
          * 
          */
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             WriteLine("This program will parse and index your json data to Elasticsearch intance.");
             WriteLine("Press any key to continue...");
             ReadKey();
 
-            ConnectionSettings connection = new ConnectionSettings();
+            ConnectionSettings connection = new ConnectionSettings()
+                .PrettyJson()
+                .DefaultIndex(args[2]);
 
             string mgmntJsonFile = args[0];
             string propertiesJsonFile = args[1];
+            string indexName = args[2];
 
-            if (args.Length > 2)
+            /* push properties and management entities to the same index named `documents` */
+
+
+            var client = new ElasticClient(connection);
+
+            #region Parse JSON files
+            Write("Parsing files...");
+            var managementRoots = JsonSerializer.Create().Deserialize<Document[]>(
+                new JsonTextReader(
+                        new StringReader(File.ReadAllText(mgmntJsonFile))
+                    )
+                );
+
+            var propertyRoots = JsonSerializer.Create(new JsonSerializerSettings()
             {
-                connection = new ConnectionSettings(new Uri(args[2]));
-            }
-            
+                FloatParseHandling = FloatParseHandling.Double
+            }).Deserialize<Document[]>(
+                new JsonTextReader(
+                        new StringReader(File.ReadAllText(propertiesJsonFile))
+                    )
+                );
+            WriteLine("DONE!");
+            #endregion
 
-            /* push properties and management entities to the same index named `default` */
-            connection.DefaultIndex("default")
-                      .PrettyJson();
+            #region Upload data to Elastic instance
+            WriteLine("Uploading Managements documents...");
 
-            try
+            var indexingService = new IndexingService(client);
+
+            Write($"Creating index '{indexName}'...");
+            await indexingService.CreateIndex(indexName);
+            WriteLine("DONE!");
+
+            var managements = managementRoots?.Where(d => d.Id != null).ToArray();
+
+            foreach (var item in managements)
             {
-                var client = new ElasticClient(connection);
+                var mResult = await indexingService.IndexDocument(item);
 
-                var managements = JsonSerializer.Create().Deserialize<ManagementRoot[]>(
-                    new JsonTextReader(
-                            new StringReader(File.ReadAllText(mgmntJsonFile))
-                        )
-                    );
-
-                var properties = JsonSerializer.Create(new JsonSerializerSettings() { 
-                    FloatParseHandling = FloatParseHandling.Double
-                }).Deserialize<PropertyRoot[]>(
-                    new JsonTextReader(
-                            new StringReader(File.ReadAllText(propertiesJsonFile))
-                        )
-                    );
-
-                ReadKey();
+                if (mResult.IsValid)
+                {
+                    WriteLine($"Document Id '{mResult.Id}' uploaded successfully!");
+                }
+                else
+                {
+                    WriteLine($"Document Id '{mResult.Id}' FAILED!");
+                }
             }
-            catch (Exception ex)
+
+            Write("Uploading Properties documents...");
+
+            var properties = propertyRoots.Where(d => d.Id != null).ToArray();
+
+            foreach (var item in properties)
             {
-                Error.WriteLine($"Error: {ex.Message}");
+                var mResult = await indexingService.IndexDocument(item);
+
+                if (mResult.IsValid)
+                {
+                    WriteLine($"Document Id '{mResult.Id}' uploaded successfully!");
+                }
+                else
+                {
+                    WriteLine($"Document Id '{mResult.Id}' FAILED!");
+                }
             }
+            #endregion
+
+            WriteLine("Tasks completed successfully.");
+            ReadKey();
+
         }
     }
 }
