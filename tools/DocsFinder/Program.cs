7f13d78c18d7f681f94c9fb0f4060f2b50bcd9c0
redouane <redouane.sobaihi@live.com>
2021-03-12 19:47:58 +0700

    feat: search by keyword and markets (WIP)

--- tools/DocsFinder/Program.cs
+++ tools/DocsFinder/Program.cs
@@ -1,68 +1,103 @@
-﻿namespace DocsFinder
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Nest;
+using Newtonsoft.Json;
+using SmartApartment.Common.Domains;
+
+using static System.Console;
+
+string uri = "http://localhost:9200";
+string index = "documents";
+int size = 25;
+
+if (args.Length > 0)
 {
-    using System;
-    using System.Threading.Tasks;
-
-    using Elasticsearch.Net;
-
-    using Nest;
-
-    using SmartApartment.Common.Domains;
-
-    using static System.Console;
-
-    class Program
+    try
     {
-        static async Task Main(string[] args)
-        {
-            ConnectionSettings connection = new ConnectionSettings();
+        uri = args[0];
+        index = args[1];
+    }
+    catch { }
+}
 
-            if (args.Length == 1)
-            {
-                connection = new ConnectionSettings(new Uri(args[0]));
-            }
+var connection = new ConnectionSettings(new Uri(uri))
+    .DefaultIndex(index)
+    .PrettyJson();
 
+var client = new ElasticClient(connection);
 
-            /* push properties and management entities to the same index named `default` */
-            connection.DefaultIndex("default")
-                      .PrettyJson();
-
-            var client = new ElasticClient(connection);
-
-            string keyword = "oak";
-            string market = "San Francisco";
-
-            var result = await client.SearchAsync<Document>(
-                s => s.Index("documents")
-                        .Size(25)
-                        .Query(
-                            q => q.MultiMatch(
-                                m => m.Fields(
-                                    f => f.Field(e => e.Management.Name)
-                                          .Field(e => e.Property.Name)
-                                ).Query(keyword)
-                            )
-                        ));
+string keyword = "bell";
+string[] markets = { "Sacramento", "Atlanta" };
 
 
-            /*
-            var result = await client.SearchAsync<Document>(s =>
-                s.Query(
-                    q => q.MultiMatch(
-                        m => m.Query("oak")
-                    )
-                )
-            );
-            */
-
-            if (!result.IsValid)
-            {
-                WriteLine("Error.");
-            }
-
-            WriteLine($"Found {result.Documents.Count} document(s).");
-
-            
-        }
+IEnumerable<Func<QueryContainerDescriptor<Document>, QueryContainer>>
+ marketsQueryGenerator(string[] markets)
+{
+    foreach (var market in markets)
+    {
+        yield return new Func<QueryContainerDescriptor<Document>, QueryContainer>(
+            q => q.MultiMatch(
+                f => f.Fields(
+                    m => m.Field(e => e.Property.Market)
+                        .Field(e => e.Management.Market)
+                ).Query(market)
+            )
+        );
     }
 }
+
+var multiMatchQuery = marketsQueryGenerator(markets).ToList();
+Func<QueryContainerDescriptor<Document>, QueryContainer> keywordQuery =
+    q => q.MultiMatch(
+        s => s.Fields(
+            f => f.Field(e => e.Property.Name)
+                .Field(e => e.Management.Name)
+                .Field(e => e.Property.FormerName)
+                .Field(e => e.Property.StreetAddress)
+                .Field(e => e.Property.State)
+                .Field(e => e.Management.State)
+        ).Query(keyword)
+    );
+
+Func<SearchDescriptor<Document>, ISearchRequest> selector = s =>
+s.Size(size)
+ .Query(
+    q => q.Bool(
+        dm => dm.Filter(
+            
+            q => q.Bool(
+                b => b.Should(multiMatchQuery)
+            ),
+            
+            keywordQuery
+        )
+    )
+    
+);
+
+var result = await client.SearchAsync<Document>(selector);
+
+if (!result.IsValid)
+{
+    WriteLine("Error.");
+}
+
+var sb = new StringBuilder();
+var settings = new JsonSerializerSettings()
+{
+    NullValueHandling = NullValueHandling.Ignore
+};
+
+foreach (var document in result.Documents)
+{
+    sb.AppendLine(JsonConvert.SerializeObject(
+        document,
+        Formatting.Indented,
+        settings
+    ));
+}
+
+WriteLine(sb.ToString());
