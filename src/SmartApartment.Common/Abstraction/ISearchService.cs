efc49e8e538f234c46cb5f8808bf55506b8eee93
redouane <redouane.sobaihi@live.com>
2021-03-12 16:01:00 +0700

    feat: implement first search logic, add cancellation token

--- src/SmartApartment.Common/Abstraction/ISearchService.cs
+++ src/SmartApartment.Common/Abstraction/ISearchService.cs
@@ -1,10 +1,17 @@
 ﻿namespace SmartApartment.Common.Abstraction
 {
+    using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
+
     using SmartApartment.Common.Domains;
 
     public interface ISearchService
     {
-        Task<Document> Search(string keyword, string market = null, int limit = 25);
+        Task<IEnumerable<Document>> Search(
+            string keyword, 
+            string market = null, 
+            int limit = 25, 
+            CancellationToken cancellationToken = default);
     }
 }
--- src/SmartApartment.Common/Services/SearchService.cs
+++ src/SmartApartment.Common/Services/SearchService.cs
@@ -1,7 +1,10 @@
 ﻿namespace SmartApartment.Common.Services
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
+
     using Nest;
     using SmartApartment.Common.Abstraction;
     using SmartApartment.Common.Domains;
@@ -15,9 +18,22 @@
             this.client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
-        public Task<Document> Search(string keyword, string market = null, int limit = 25)
+        public async Task<IEnumerable<Document>> Search(string keyword,
+                                                  string market = null,
+                                                  int limit = 25,
+                                                  CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            var response = await this.client.SearchAsync<Document>(
+                s => s.Size(limit)
+                      .QueryOnQueryString(keyword)
+            , cancellationToken);
+
+            if (!response.IsValid)
+            {
+                throw new Exception("Unable to process search operation.");
+            }
+
+            return response.Documents;
         }
     }
 }
